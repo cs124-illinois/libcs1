6c6c971bd1a9f5902b91be0ed38ad832facea21e
Geoffrey Challen <challen@illinois.edu>
2021-05-10 11:18:04 -0500

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.cs125-illinois.libcs1"
-version = "2021.4.1"
+version = "2021.5.0"
 
 plugins {
-    kotlin("jvm") version "1.4.32"
+    kotlin("jvm") version "1.5.0"
     `maven-publish`
-    id("org.jmailen.kotlinter") version "3.4.0"
+    id("org.jmailen.kotlinter") version "3.4.4"
     id("com.github.ben-manes.versions") version "0.38.0"
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
 }
