bac7e006f74d650fadddfb192a40528af2c4d2b1
Geoffrey Challen <challen@illinois.edu>
2021-08-30 09:05:42 -0500

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,16 @@
 group = "com.github.cs125-illinois"
-version = "2021.8.0"
+version = "2021.8.1"
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
     `maven-publish`
-    id("org.jmailen.kotlinter") version "3.5.0"
+    id("org.jmailen.kotlinter") version "3.5.1"
     id("com.github.ben-manes.versions") version "0.39.0"
-    id("io.gitlab.arturbosch.detekt") version "1.18.0"
+    id("io.gitlab.arturbosch.detekt") version "1.18.1"
 }
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
-}
-dependencies {
-    implementation(kotlin("stdlib"))
 }
 tasks.dependencyUpdates {
     fun String.isNonStable() = !(
