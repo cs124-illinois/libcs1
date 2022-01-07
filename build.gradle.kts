ef15142f232042b45236a461aeee639ada8766f7
Geoffrey Challen <challen@illinois.edu>
2022-01-07 13:09:34 -0600

    Dependency updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.cs125-illinois"
-version = "2021.12.2"
+version = "2022.1.0"
 
 plugins {
     kotlin("jvm") version "1.6.10"
     `maven-publish`
     id("org.jmailen.kotlinter") version "3.8.0"
-    id("com.github.ben-manes.versions") version "0.39.0"
+    id("com.github.ben-manes.versions") version "0.41.0"
     id("io.gitlab.arturbosch.detekt") version "1.19.0"
 }
 repositories {
