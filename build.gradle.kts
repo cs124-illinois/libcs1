7e7f02bb94486b73ef3d444f1fef8ff555ee92db
Geoffrey Challen <challen@illinois.edu>
2023-10-06 10:48:22 -0500

    Latest deps.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.cs124-illinois"
-version = "2023.8.0"
+version = "2023.10.0"
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     `maven-publish`
     id("org.jmailen.kotlinter") version "3.16.0"
-    id("com.github.ben-manes.versions") version "0.47.0"
+    id("com.github.ben-manes.versions") version "0.48.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
 }
 repositories {
     mavenCentral()
 }
 dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-    testImplementation("org.slf4j:slf4j-simple:2.0.7")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("org.slf4j:slf4j-simple:2.0.9")
 }
 tasks.dependencyUpdates {
     fun String.isNonStable() = !(
