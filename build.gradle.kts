633ad03809d56780e61684125cd0ca2484938407
Geoffrey Challen <challen@illinois.edu>
2024-08-05 10:14:47 -0500

    Bump version for publish.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.cs124"
-version = "2024.6.0"
+version = "2024.8.0"
 
 plugins {
     kotlin("jvm") version "2.0.0"
